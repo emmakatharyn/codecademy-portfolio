a8dcd1d13051e5abd9fb49cd0d3cc8e388b5dc62
Emma Reed <ereed@easitpa.com>
2023-01-24 14:55:04 -0700

    fix colormode button to toggle text properly

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ const lightSwitch = document.querySelector("#colorToggle");
 
 lightSwitch.addEventListener("click", function () {
   page.classList.toggle("dark");
+  if (lightSwitch.innerHTML == "Dark mode") {
+    lightSwitch.innerHTML = "Light mode";
+  } else if (lightSwitch.innerHTML == "Light mode") {
+    lightSwitch.innerHTML = "Dark mode";
+  }
 });
