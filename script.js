dca5459bd3e21af7c110bafc1ce71e441472c03a
Emma Reed <ereed@easitpa.com>
2023-03-19 19:14:38 -0600

    change theme switch to icons; some other stuff in the hero i think... idk it was forever ago

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ const lightSwitch = document.querySelector("#colorToggle");
 
 lightSwitch.addEventListener("click", function () {
   page.classList.toggle("dark");
-  if (lightSwitch.innerHTML == "Dark theme") {
-    lightSwitch.innerHTML = "Light theme";
+  console.log("helloooo");
+  if (page.classList.contains("dark")) {
+    console.log("You are using Dark theme");
+    lightSwitch.innerHTML = `<ion-icon class='color-theme-icon' name='sunny'></ion-icon>`;
     lightSwitch.style.backgroundColor = "#fff";
-  } else if (lightSwitch.innerHTML == "Light theme") {
-    lightSwitch.innerHTML = "Dark theme";
+  } else if (!page.classList.contains("dark")) {
+    console.log("You are using light theme");
+    lightSwitch.innerHTML = `<ion-icon class='color-theme-icon' name='moon'></ion-icon>`;
     lightSwitch.style.backgroundColor = "rgb(57, 31, 246)";
   }
+
+  // if (lightSwitch.innerHTML == "<ion-icon name='moon'></ion-icon>") {
+  //   lightSwitch.innerHTML = "<ion-icon name='sunny'></ion-icon>";
+  //   lightSwitch.style.backgroundColor = "#fff";
+  // } else if (lightSwitch.innerHTML == "<ion-icon name='sunny'></ion-icon>") {
+  //   lightSwitch.innerHTML = '<ion-icon name="moon"></ion-icon>';
+  //   lightSwitch.style.backgroundColor = "rgb(57, 31, 246)";
+  // }
 });
